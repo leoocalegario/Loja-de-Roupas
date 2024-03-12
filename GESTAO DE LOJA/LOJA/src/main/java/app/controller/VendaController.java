3ed52d1718c65799ef5a56f5a50c7d6207e4df9c
ary <arineto10@hotmail.com>
2024-03-12 19:28:35 -0300

    atualizacao

--- GESTAO DE LOJA/LOJA/src/main/java/app/controller/VendaController.java
+++ GESTAO DE LOJA/LOJA/src/main/java/app/controller/VendaController.java
@@ -1,5 +1,13 @@
 package app.controller;
 
+
+@RestController
+@RequestMapping("/api/loja")
 public class VendaController {
 
+	@Autowired
+	private VendaService vendaService;
+	
+	@PostMapping("/save")
+	public ResponseEntity<String> save(@RequestBody Venda venda)
 }
