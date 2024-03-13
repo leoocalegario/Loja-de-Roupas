c9ab0e05b3b6ecfcbd3c4ff3b66040c843799964
Vini Casseb <vinicasseb99@gmail.com>
2024-03-12 22:33:08 -0300

    excluindo arquivos

--- GESTAO DE LOJA/LOJA/src/main/java/app/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	
-}
